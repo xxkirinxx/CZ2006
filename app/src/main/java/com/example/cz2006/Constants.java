067341c18fbf518e4850a9e77c157fce795b06b6
Daryl Neo <darylneojiaxun@hotmail.com>
2021-03-30 01:26:24 +0800

    Combined Kai's Report function

--- app/src/main/java/com/example/cz2006/Constants.java
+++ app/src/main/java/com/example/cz2006/Constants.java
@@ -4,4 +4,5 @@ public class Constants {
     public static final int ERROR_DIALOG_REQUEST = 9001;
     private static final String MAPVIEW_BUNDLE_KEY="MapViewBundleKey";
     public static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 9002;
+    public static final int PERMISSIONS_REQUEST_ACCESS_CAMERA = 9003;
 }
