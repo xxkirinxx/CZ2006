8589bdef79c47174aecdbd840150c4f98b4bbaee
klee045 <leekaien98@gmail.com>
2021-04-06 15:41:54 +0800

    added firebase for report submission

--- app/src/main/java/com/example/cz2006/Controller/ReportActivity.java
+++ app/src/main/java/com/example/cz2006/Controller/ReportActivity.java
@@ -1,6 +1,7 @@
 package com.example.cz2006.Controller;
 
 import android.Manifest;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -10,6 +11,7 @@ import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Base64;
@@ -21,6 +23,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
@@ -29,11 +32,22 @@ import com.example.cz2006.Entity.Report;
 import com.example.cz2006.R;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnPausedListener;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageMetadata;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.List;
 import java.util.Locale;
+import java.util.UUID;
 
 import static android.Manifest.permission.ACCESS_FINE_LOCATION;
 import static com.example.cz2006.Constants.PERMISSIONS_REQUEST_ACCESS_CAMERA;
@@ -49,6 +63,7 @@ public class ReportActivity extends AppCompatActivity implements LocationListene
     private TextView locationTextView;
 
     private boolean tappedBtnGetLoc = false;
+    private boolean uploadFailed = false;
     private Report report = new Report();
 
     @Override
@@ -60,19 +75,17 @@ public class ReportActivity extends AppCompatActivity implements LocationListene
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
         locationTextView = findViewById(R.id.locationTextView);
         Button BtnGetLoc = findViewById(R.id.BtnGetLoc);
-
+        try {
+            locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            checkPermission();
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, ReportActivity.this);
+        } catch (Exception e) {
+            Log.d("error", e.getMessage());
+            return;
+        }
         BtnGetLoc.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                try {
-                    locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-                    checkPermission();
-                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, ReportActivity.this);
-                } catch (Exception e) {
-                    Log.d("error", e.getMessage());
-                    return;
-                }
-
                 if (loc == null) {
                     Toast.makeText(ReportActivity.this,
                             "Please try again in a few seconds. If the problem still persists, please fix your GPS.",
@@ -81,7 +94,7 @@ public class ReportActivity extends AppCompatActivity implements LocationListene
                 }
                 report.setLocation(loc);
                 tappedBtnGetLoc = true;
-                locationTextView.setText(String.format("Latitude: %s, Longitude: %s", report.getLatitude(), report.getLongitude()));
+                locationTextView.setText("Current Location:\n" + getAddress(report.getLatitude(), report.getLongitude()));
             }
         });
 
@@ -99,6 +112,14 @@ public class ReportActivity extends AppCompatActivity implements LocationListene
 
         editTextMulti = findViewById(R.id.editTextTextMultiLine);
 
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+        // check if it is the right storage bucket
+        FirebaseOptions opts = FirebaseApp.getInstance().getOptions();
+        Log.i("er", "Bucket = " + opts.getStorageBucket());
+        String uniqueId = UUID.randomUUID().toString();  // Generate UUID
+        StorageReference storageRef = storage.getReference();
+        StorageReference imagesRef = storageRef.child("reports/" + uniqueId + "/image.png");
+
         // Submit Button Actions
         btnSubmit = findViewById(R.id.button4);
         btnSubmit.setOnClickListener(new View.OnClickListener() {
@@ -107,20 +128,71 @@ public class ReportActivity extends AppCompatActivity implements LocationListene
                 // check if report still has missing fields
                 if (reportNotComplete()) return;
 
-                // convert bitmap to string for logging
+                // convert image bitmap to string for logging
                 ByteArrayOutputStream baos = new ByteArrayOutputStream();
                 report.getBitmap().compress(Bitmap.CompressFormat.PNG, 100, baos);
                 byte[] b = baos.toByteArray();
                 String temp = Base64.encodeToString(b, Base64.DEFAULT);
 
-                // Log all for now. To upload to database.
-                Log.d("Photo", temp);
-                Log.d("Report Details", report.getDetails());
-                Log.d("Location", getAddress(report.getLatitude(), report.getLongitude()));
-
                 Toast.makeText(ReportActivity.this,
                         "Submitting Report...",
                         Toast.LENGTH_SHORT).show();
+
+                // Upload Image
+                UploadTask uploadTask = imagesRef.putBytes(b);
+                uploadTask.addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception exception) {
+                        Log.d("Failed", "Image failed to upload");
+                        uploadFailed = true;
+                    }
+                }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        Log.d("Succeed", "Image uploaded");
+                    }
+                }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                        double progress = (100.0 * taskSnapshot.getBytesTransferred()) / taskSnapshot.getTotalByteCount();
+                        Log.d("Progress", "Upload is " + progress + "% done");
+                    }
+                }).addOnPausedListener(new OnPausedListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onPaused(UploadTask.TaskSnapshot taskSnapshot) {
+                        Log.d("Paused", "Upload is paused");
+                    }
+                });;
+
+                // Upload Details & Location
+                StorageReference detailRef = storageRef.child("reports/" + uniqueId + "/details.txt");
+                String details = "Location: " + getAddress(report.getLatitude(), report.getLongitude())
+                        + "\n"
+                        + "Latitude: " + report.getLatitude() + ", Longitude: " + report.getLongitude()
+                        + "\n\n"
+                        + "Details:\n"
+                        + report.getDetails();
+                detailRef.putBytes(details.getBytes()).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        Log.d("Succeed", "Uploaded details successfully");
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d("Failed", "Details failed to upload");
+                        uploadFailed = true;
+                    }
+                });
+
+                // if upload failed, exit
+                if (uploadFailed) {
+                    Toast.makeText(ReportActivity.this,
+                            "Upload has failed. Please try again later.",
+                            Toast.LENGTH_SHORT).show();
+                    uploadFailed = false;  // reset bool
+                    return;
+                }
 
                 // reset values after submitting form
                 editTextMulti.setText("");
@@ -204,19 +276,21 @@ public class ReportActivity extends AppCompatActivity implements LocationListene
 
     // convert latitude & longitude into actual address
     private String getAddress(double latitude, double longitude) {
-        StringBuilder result = new StringBuilder();
+        Geocoder geocoder;
+        List<Address> addresses = null;
+        geocoder = new Geocoder(this, Locale.getDefault());
+
         try {
-            Geocoder geocoder = new Geocoder(this, Locale.getDefault());
-            List<Address> addresses = geocoder.getFromLocation(latitude, longitude, 1);
-            if (addresses.size() > 0) {
-                Address address = addresses.get(0);
-                result.append(address.getLocality()).append("\n");
-                result.append(address.getCountryName());
-            }
+            addresses = geocoder.getFromLocation(latitude, longitude, 1);
         } catch (IOException e) {
-            Log.e("tag", e.getMessage());
+            e.printStackTrace();
         }
 
-        return result.toString();
+        String address = addresses.get(0).getAddressLine(0);
+        
+        Log.d("address", address);
+        return address;
     }
+
+
 }
