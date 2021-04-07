deb22f73a8ee180204087f93e247ecf6d33afca2
KooJianYang <jianyangkoo@gmail.com>
2021-04-07 22:29:15 +0800

    Profile Update to Change Password

--- app/src/main/java/com/example/cz2006/Controller/ProfileActivity.java
+++ app/src/main/java/com/example/cz2006/Controller/ProfileActivity.java
@@ -1,5 +1,6 @@
 package com.example.cz2006.Controller;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,8 +8,11 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.cz2006.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -46,8 +50,19 @@ public class ProfileActivity extends AppCompatActivity {
     }
 
     public void launchChangePW(View v) {
-//        Intent i = new Intent(this, com.example.cz2006.Controller.ChangePasswordActivity.class);
-//        startActivity(i);
+        String email = LoginActivity.user.getEmail();
+        FirebaseAuth.getInstance().sendPasswordResetEmail(email)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(ProfileActivity.this, "Password Change Link Sent To Your Email", Toast.LENGTH_SHORT).show();
+                        }
+                        else {
+                            Toast.makeText(ProfileActivity.this, "Error. Please Try Again Later", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 
     public void launchHome(View v) {
