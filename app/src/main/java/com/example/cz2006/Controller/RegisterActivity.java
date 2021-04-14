d52d63cad1679f7f5d1b7e97a9bd4c9beb947da1
Daryl Neo <darylneojiaxun@hotmail.com>
2021-04-14 17:36:47 +0800

    Update some stuffs

--- app/src/main/java/com/example/cz2006/Controller/RegisterActivity.java
+++ app/src/main/java/com/example/cz2006/Controller/RegisterActivity.java
@@ -1,8 +1,5 @@
 package com.example.cz2006.Controller;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -10,12 +7,14 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.example.cz2006.R;
-import com.google.firebase.auth.FirebaseAuth;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.cz2006.R;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserProfileChangeRequest;
 
